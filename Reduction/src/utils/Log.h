40772a9d9f2b93e191389b3cf6d42858310f06ff
Sahil Iyer <iyersahil12@gmail.com>
2019-05-31 14:05:21 +1000

    Added non-debug mode log functions.

--- Reduction/src/utils/Log.h
+++ Reduction/src/utils/Log.h
@@ -5,24 +5,33 @@
 #include <iostream>
 
 template<typename... Args>
-void info(Args&& ... args)
+void info(Args&&... args)
 {
 	std::cout << "[INFO] - ";
 	(std::cout << ... << args) << std::endl;
 }
 
 template<typename... Args>
-void warn(Args && ... args)
+void warn(Args&&... args)
 {
 	std::cout << "[WARNING] - ";
 	(std::cout << ... << args) << std::endl;
 }
 
 template<typename... Args>
-void error(Args && ... args)
+void error(Args&&... args)
 {
 	std::cout << "[ERROR] - ";
 	(std::cout << ... << args) << std::endl;
 }
+
+#else
+
+template<typename... Args>
+void info(Args&&...) {}
+template<typename... Args>
+void warn(Args&&...) {}
+template<typename... Args>
+void error(Args&&...) {}
 
 #endif  // _DEBUG
